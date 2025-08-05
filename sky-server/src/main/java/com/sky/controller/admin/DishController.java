20a9e4a7e14e63ce0cb7be8ebfcb9d3803c5329d
ggbond <484547374@qq.com>
2025-08-05 12:35:33 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -12,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin/dish")
 @Api(tags = "菜品相关接口")
@@ -45,5 +47,22 @@ public class DishController {
         PageResult pageResult=dishService.pageQuery(dishPageQueryDTO);
 
         return Result.success(pageResult);
+    }
+
+
+    /**
+     * 删除菜品
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    @ApiOperation("删除菜品")
+    public Result delete(@RequestParam List<Long> ids){
+
+        log.info("删除菜品:{}",ids);
+
+        dishService.deleteBatch(ids);
+
+        return Result.success();
     }
 }
