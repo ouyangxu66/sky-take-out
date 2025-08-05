dd84f5b3bafa0a8420901e494a8d44576280c8f6
ggbond <484547374@qq.com>
2025-08-05 12:47:30 +0800

    删除菜品业务代码优化

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -106,12 +106,14 @@ public class DishServiceImpl implements DishService {
         }
 
         //3.删除菜品表中的菜品数据
-        for (Long id : ids) {
-            dishMapper.deleteById(id);
-            //4.删除菜品关联的口味数据
-            dishFlavorMapper.deleteByDishId(id);
-        }
+//        for (Long id : ids) {
+//            dishMapper.deleteById(id);
+//            //4.删除菜品关联的口味数据
+//            dishFlavorMapper.deleteByDishId(id);
+//        }
 
-
+        //根据菜品id集合批量删除菜品以及批量删除关联的口味数据
+        dishMapper.deleteByIds(ids);
+        dishFlavorMapper.deleteByDishIds(ids);
     }
 }
